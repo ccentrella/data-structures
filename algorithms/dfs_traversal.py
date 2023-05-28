5bdab664f970ebc8f2d2299ee6eabc7288a9eb8a
Christopher Centrella <ccentrella.j@gmail.com>
2023-05-28 13:58:13 -0500

    Refactor to load data from graph file

--- algorithms/dfs_traversal.py
+++ algorithms/dfs_traversal.py
@@ -1,12 +1,44 @@
+import json
+import os
+from os import path
+
 class GraphEdge:
     def __init__(self, value, next_edge=None):
         self.value: int = value
         self.next_edge: GraphEdge = next_edge
 
 class Graph:
-    def __init__(self, edges, labels):
-        self.edges: [GraphEdge] = edges
-        self.labels: [int] = labels
+    def __init__(self, file_location=None):
+        self.edges: [GraphEdge] = []
+        self.labels: [int] = []
+
+        if file_location is not None:
+            self.load_file(file_location)
+
+    def load_file(self, file_location):
+        with open(file_location, 'r') as file:
+            data = file.read()
+            graph_data = json.loads(data)
+            graph_edges = graph_data['edges']
+            graph_labels = graph_data['labels']
+
+            for index, graph_edge in graph_edges.items():
+                graph_label = graph_labels[index]
+                self.add_vertex(graph_label, graph_edge)
+
+    def add_vertex(self, label, edges=None):
+        self.edges.append(None)
+        self.labels.append(label)
+
+        if edges is not None:
+            index = len(self.edges) - 1
+            for edge in edges:
+                self.add_edge(index, edge)
+
+    def add_edge(self, x, y):
+        current_edge = self.edges[x]
+        new_edge = GraphEdge(y, current_edge)
+        self.edges[x] = new_edge
 
     def start_traversal(self, root=0):
         queue: [int] = []
@@ -22,7 +54,7 @@ class Graph:
 
     def traverse(self, vertex, queue, discovered):
         current_edge = self.edges[vertex]
-        while current_edge != None:
+        while current_edge is not None:
             current_index = current_edge.value
             if not discovered[current_index]:
                 queue.append(current_index)
@@ -73,7 +105,7 @@ def define_edges() -> [GraphEdge]:
     return edges
 
 # Entrypoint
-edges = define_edges()
-labels = [101, 234, 96, 382, 9, 2, 3, 10, 72]
-graph = Graph(edges, labels)
+parent_directory = path.dirname(os.getcwd())
+graph_file = path.join(parent_directory, 'data', 'graph.json')
+graph = Graph(graph_file)
 graph.start_traversal()
--- algorithms/recursive_dfs_traversal.py
+++ algorithms/recursive_dfs_traversal.py
@@ -1,12 +1,44 @@
+import json
+import os
+from os import path
+
 class GraphEdge:
     def __init__(self, value, next_edge=None):
         self.value: int = value
         self.next_edge: GraphEdge = next_edge
 
 class Graph:
-    def __init__(self, edges, labels):
-        self.edges: [GraphEdge] = edges
-        self.labels: [int] = labels
+    def __init__(self, file_location=None):
+        self.edges: [GraphEdge] = []
+        self.labels: [int] = []
+
+        if file_location is not None:
+            self.load_file(file_location)
+
+    def load_file(self, file_location):
+        with open(file_location, 'r') as file:
+            data = file.read()
+            graph_data = json.loads(data)
+            graph_edges = graph_data['edges']
+            graph_labels = graph_data['labels']
+
+            for index, graph_edge in graph_edges.items():
+                graph_label = graph_labels[index]
+                self.add_vertex(graph_label, graph_edge)
+
+    def add_vertex(self, label, edges=None):
+        self.edges.append(None)
+        self.labels.append(label)
+
+        if edges is not None:
+            vertex = len(self.edges) - 1
+            for edge in edges:
+                self.add_edge(vertex, edge)
+
+    def add_edge(self, x, y):
+        current_edge = self.edges[x]
+        new_edge = GraphEdge(y, current_edge)
+        self.edges[x] = new_edge
 
     def start_traversal(self, root=0):
         discovered: [bool] = [False for i in range(len(self.edges))]
@@ -72,7 +104,7 @@ def define_edges() -> [GraphEdge]:
     return edges
 
 # Entrypoint
-edges = define_edges()
-labels = [6, 23, 9235, 239, 92, 256, 88, 6, 120]
-graph = Graph(edges, labels)
+parent_directory = path.dirname(os.getcwd())
+graph_file = path.join(parent_directory, 'data', 'graph.json')
+graph = Graph(graph_file)
 graph.start_traversal()
