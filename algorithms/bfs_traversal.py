96fe394c82d9f80d1506f0df59981a60717f95c4
Christopher Centrella <ccentrella.j@gmail.com>
2023-05-28 13:16:36 -0500

    Refactor BFS traversal to load data from graph file

--- algorithms/bfs_traversal.py
+++ algorithms/bfs_traversal.py
@@ -1,12 +1,44 @@
+import json
+import os
+from os import path
+
 class GraphEdge:
     def __init__(self, value, next_edge=None):
         self.value: int = value
         self.next_edge: GraphEdge = next_edge
 
 class Graph:
-    def __init__(self, edges, labels):
-        self.edges: [GraphEdge] = edges
-        self.labels: [int] = labels
+    def __init__(self, file_location=None):
+        self.edges: [GraphEdge] = []
+        self.labels: [int] = []
+
+        if file_location is not None:
+            self.load_file(file_location)
+
+    def load_file(self, file_location):
+        with open(file_location, 'r') as file:
+            data = file.read()
+            graph_data = json.loads(data)
+            graph_edges = graph_data['edges']
+            graph_labels = graph_data['labels']
+
+            for index, graph_edge in graph_edges.items():
+                graph_label = graph_labels[index]
+                self.add_vertex(graph_label, graph_edge)
+
+    def add_vertex(self, label, edges=None):
+        self.edges.append(None)
+        self.labels.append(label)
+
+        if edges is not None:
+            vertex = len(self.edges) - 1
+            for edge in edges:
+                self.add_edge(vertex, edge)
+
+    def add_edge(self, x, y):
+        current_edge = self.edges[x]
+        new_edge = GraphEdge(y, current_edge)
+        self.edges[x] = new_edge
 
     def start_traversal(self, root=0):
         queue: [int] = []
@@ -73,7 +105,7 @@ def define_edges() -> [GraphEdge]:
     return edges
 
 # Entry point
-vertices = define_edges()
-labels = [9, 25, 23, 92, 58, 27, 18, 29, 6]
-graph = Graph(vertices, labels)
+parent_directory = os.path.dirname(os.getcwd())
+graph_file = path.join(parent_directory, 'data', 'graph.json')
+graph = Graph(graph_file)
 graph.start_traversal()
